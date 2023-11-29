ce1fa42ab5cae6cee50e8c4b255d21193fd7cca7
zechaoxv <55899243+zechaoxv@users.noreply.github.com>
2023-11-29 16:01:53 -0500

    Added seq plot functions

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
+import torch
+import pandas as pd
+from skimage import io, transform
+import numpy as np
 import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+
 
 def plot_losses(train_losses, val_losses):
     plt.figure(figsize=(10, 5))
@@ -19,3 +25,18 @@ def plot_column(data, col_name):
     plt.grid(True)
     plt.show()
 
+def plot_output(y_pred_seq, y_true_seq, seq_len):
+    pred_arr = []
+    true_arr = []
+    for i in range(0,len(y_pred_seq),seq_len):
+        pred_arr.append(y_pred_seq[i])
+        true_arr.append(y_true_seq[i])
+    plt.figure(figsize=(10, 5))
+    plt.plot(pred_arr, label='prediction_seq')
+    plt.plot(true_arr, label='GndTruth_seq')
+    plt.title('pred vs groundtruth')
+    plt.xlabel('Seq index')
+    plt.ylabel('Energy Consumed')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
